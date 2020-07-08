224db5606166554426deac56a9a418ca44ea1c51
Ashley Anderson <a.anderson16@student.unimelb.edu.au>
2020-07-08 15:51:50 +1000

    start test file
    initiate 1x pass and 1x fail test

---
+++ tests.py
@@ -0,0 +1,12 @@
+import pytest
+import tictactoe
+
+def test_always_passes() -> None:
+    assert True, "This should pass"
+
+
+def test_always_fails() -> None:
+    assert False, "This should fail"
+
+
+# TODO: Test the final play function.
