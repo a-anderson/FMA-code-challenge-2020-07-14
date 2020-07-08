9fea320f31bcebb6cf8603978f696839d1a37b54
Ashley Anderson <a.anderson16@student.unimelb.edu.au>
2020-07-08 13:31:55 +1000

    add basic play structure

---
+++ tictactoe.py
@@ -0,0 +1,23 @@
+
+# Game Structure
+# player = 'X'/1
+# winner = None
+# display instructions
+#
+# while True:
+#     player instruction message
+#     console input
+#         --> check for [Qq] --> quit game --> display victory by default message
+#         --> check valid coordinate
+#         --> check coordinate not taken
+#     convert to actionable data format
+#     place entry onto board
+#     display board
+#     check win condition --> winner = player --> break
+#         --> row victory
+#         --> column victory
+#         --> diagonal victory
+#     check if the board is full --> if full --> break
+#     switch players
+#
+# print game results to console
