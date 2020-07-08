757a056cc808639287650a444724ef45918c9f69
Ashley Anderson <a.anderson16@student.unimelb.edu.au>
2020-07-08 17:16:58 +1000

    refactor tests to reflect move_is_valid change in tictactoe.py

--- tests.py
+++ tests.py
@@ -9,15 +9,22 @@ def test_always_fails() -> None:
     assert False, "This should fail"
 
 
-def test_move_is_valid_coordinates_pass() -> None:
-    assert tictactoe.move_is_valid("1,3") == True, "Accepts good co-ordinate input"
-
-
-def test_move_is_valid_q_pass() -> None:
-    assert tictactoe.move_is_valid("q") == True and tictactoe.move_is_valid("Q") == True, "Accepts quit"
+def test_move_is_valid_true() -> None:
+    assert tictactoe.move_is_valid("1,3") == True, "Accepts valid player input"
 
 
 def test_move_is_valid_fail() -> None:
     assert tictactoe.move_is_valid("1,4") == False, "Rejects invalid player input"
+
+
+def test_player_quits_true() -> None:
+    assert tictactoe.player_quits("q") == True and tictactoe.player_quits("Q") == True, "Accepts quit"
+
+
+def test_player_quits_false() -> None:
+    assert tictactoe.player_quits("no") == False and tictactoe.player_quits("1,2") == False, "Rejects anything but [Qq] input"
+
+
+
 
 # TODO: Test the final play function.
