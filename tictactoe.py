68d76b3d448059616ab2f1c521f1f9d45a374b2c
Ashley Anderson <a.anderson16@student.unimelb.edu.au>
2020-07-08 15:52:57 +1000

    add global variables & game setup messages

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,3 @@
-
 # Game Structure
 # player = 'X'/1
 # winner = None
@@ -21,3 +20,72 @@
 #     switch players
 #
 # print game results to console
+
+COORDINATES = {"1,1", "1,2", "1,3",
+               "2,1", "2,2", "2,3",
+               "3,1", "3,2", "3,3"}
+
+PLAYER_SYMBOLS = {
+    1: "X",
+    2: "O"
+}
+
+OPPONENTS = {
+    1: 2,
+    2: 1
+}
+
+def welcome_message() -> None:
+    print("Welcome to Ashley's 2-player Tic Tac Toe!"
+          "\n\tGet ready to have some fun!"
+          "\n\nHere is the board layout:")
+
+def generate_board() -> dict:
+    """
+    Generates an empty board layout
+    :return: dict: primary board setup.
+    """
+    board = {
+        "1,1": "_",
+        "1,2": "_",
+        "1,3": "_",
+        "2,1": "_",
+        "2,2": "_",
+        "2,3": "_",
+        "3,1": "_",
+        "3,2": "_",
+        "3,3": "_",
+    }
+    return board
+
+
+def print_board(board: dict) -> None:
+    print("\n\tYour Board" + "\t"*5 + "Coordinates\n"
+          f"\n|\t{board['1,1']}\t{board['1,2']}\t{board['1,3']}\t|"
+          "\t\t|\t(1,1)\t(1,2)\t(1,3)\t|" # end of line 1
+          f"\n|\t{board['2,1']}\t{board['2,2']}\t{board['2,3']}\t|"
+          "\t\t|\t(2,1)\t(2,2)\t(2,3)\t|" # end of line 2
+          f"\n|\t{board['3,1']}\t{board['3,2']}\t{board['3,3']}\t|"
+          "\t\t|\t(3,1)\t(3,2)\t(3,3)\t|\n" # end of line 3
+          )
+
+def player_instructions(player: int) -> str:
+    print(f"Player {player}, enter coordinates row,column to place your {PLAYER_SYMBOLS[player]}" 
+          "\n\tor enter q to quit the game.")
+
+
+def play() -> None:
+    '''
+    Game algorithm for a simple 2-player CLI Tic Tac Toe game.
+    :return: None
+    '''
+    welcome_message()
+    board = generate_board()
+    print_board(board)
+    active_player = 1
+    winner = None
+    while True:
+        player_instructions(active_player)
+        break
+
+    pass
