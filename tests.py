8f795e0b3dd55e8b1d017241a1794685c0d903e7
Ashley Anderson <a.anderson16@student.unimelb.edu.au>
2020-07-10 11:35:35 +1000

    refactor tests with pytest parameterize decorator

--- tests.py
+++ tests.py
@@ -1,30 +1,22 @@
 import pytest
 import tictactoe
 
-
-def test_move_is_valid_true() -> None:
-    assert tictactoe.move_is_valid("1,3", tictactoe.generate_board()) == True, "Accepts valid player input"
-
-
-def test_move_is_valid_fail() -> None:
-    assert tictactoe.move_is_valid("1,4", tictactoe.generate_board()) == False, "Rejects invalid player input"
-
-
-def test_move_is_valid_square_taken() -> None:
+@pytest.mark.parametrize("input_coordinate, expected_output, output_str",
+                         [("1,3", True, "Accepts valid player input"),
+                          ("1,4", False, "Rejects invalid player input"),
+                          ("1,1", False, "Rejects if square already taken")])
+def test_move_is_valid(input_coordinate, expected_output, output_str) -> None:
     board = tictactoe.generate_board()
-    player_move = "1,3"
-    board[player_move] = "X"
-    assert tictactoe.move_is_valid(player_move, board) == False, "Rejects input if square not free"
+    occupied_square = "1,1"
+    board[occupied_square] = 'X'
+    assert tictactoe.move_is_valid(input_coordinate, board) == expected_output, \
+        output_str
 
 
-def test_player_quits_true() -> None:
-    assert tictactoe.player_quits("q") == True and \
-           tictactoe.player_quits("Q") == True, "Accepts quit"
-
-
-def test_player_quits_false() -> None:
-    assert tictactoe.player_quits("no") == False and \
-           tictactoe.player_quits("1,2") == False, "Rejects anything but [Qq] input"
+@pytest.mark.parametrize("player_input, expected_output",
+                         [("q", True), ("Q", True), ("no", False), ("1,2", False)])
+def test_player_quits(player_input, expected_output) -> None:
+    assert tictactoe.player_quits(player_input) == expected_output, "Correctly accepts/rejects quit input"
 
 
 def test_is_winner_return_true_if_winning_sequence() -> None:
@@ -42,36 +34,30 @@ def test_is_winner_return_false_if_not_winning_sequence() -> None:
     assert tictactoe.is_winner("1,3", board) == False, "Returns False for non-winning move"
 
 
-def test_end_of_game_message_winner(capsys) -> None:
-    winner = 2
+@pytest.mark.parametrize("winner, expected_output, output_str",
+                         [(None, f"The game is a draw - an even match!\n", "Prints no winner message if draw"),
+                          (2, (f"!!PLAYER 2 HAS WON THE GAME!!\n"
+                               f"!!CONGRATULATIONS PLAYER 2!!\n"),
+                           "Prints winning message if winner")])
+def test_end_of_game_message(capsys, winner, expected_output, output_str) -> None:
     tictactoe.end_of_game_message(winner)
     out, err = capsys.readouterr()
-    assert out == (f"!!PLAYER {winner} HAS WON THE GAME!!\n"
-              f"!!CONGRATULATIONS PLAYER {winner}!!\n"), "Prints winning message if winner"
+    assert out == expected_output, output_str
 
-
-def test_end_of_game_message_draw(capsys) -> None:
-    tictactoe.end_of_game_message(None)
-    out, err = capsys.readouterr()
-    assert out == (f"The game is a draw - an even match!\n"), "Prints no winner message if draw"
-
-# TODO: Test the final play function.
 
 @pytest.mark.parametrize("input_coordinates, expected_output",
                          [(["1,1", "2,2", "2,1", "1,3", "3,1"], "!!CONGRATULATIONS PLAYER 1!!\n"),
                           (["1,1", "2,2", "2,1", "1,3", "1,2", "3,1"], "!!CONGRATULATIONS PLAYER 2!!\n"),
                           (["2,2", "1,2", "1,3", "1,1", "2,1", "2,3", "3,2", "3,1", "3,3"], "The game is a draw - an even match!\n")
                           ])
-def test_play_player1_wins(capsys, input_coordinates, expected_output) -> None:
+def test_play(capsys, input_coordinates, expected_output) -> None:
 
     def mock_input(s):
         return input_coordinates.pop(0)
-    tictactoe.input = mock_input
 
+    tictactoe.input = mock_input
     tictactoe.play()
     out, err = capsys.readouterr()
-
     str_length = len(expected_output)
-
-    assert out[-str_length:] == expected_output, "Play prints winning player"
+    assert out[-str_length:] == expected_output, "Play correctly prints winning player"
 
