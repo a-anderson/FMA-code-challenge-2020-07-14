c7c6b8895e733137030dd21e0570974adef78d9f
Ashley Anderson <a.anderson16@student.unimelb.edu.au>
2020-07-11 17:08:53 +1000

    update docstrings for tictactoe functions

--- tictactoe.py
+++ tictactoe.py
@@ -29,9 +29,31 @@ WIN_LOCATIONS = {
 DIVIDER = f"{'=' * 40}"
 
 
+def print_divider(time_delay: bool = False) -> None:
+    '''
+    Prints a row of "=" to divide game rounds, add
+    emphasis to an outcome, or show a loading bar.
+    :param time_delay: bool: Use a time delay to print the divider
+    :return: None
+    '''
+    if time_delay:
+        for char in DIVIDER:
+            print(char, end="", flush=True)
+            time.sleep(0.015)
+        print("\n")
+    else:
+        print(DIVIDER)
+
+
 def welcome_message() -> None:
+    '''
+    Print a welcome message to the console.
+    :return: None
+    '''
+    print_divider()
     print("Welcome to Ashley's 2-player Tic Tac Toe!"
           "\n\tGet ready to have some fun!")
+    print_divider()
 
 
 def generate_board() -> dict:
@@ -53,8 +75,13 @@ def generate_board() -> dict:
 
 
 def print_board(board: dict) -> None:
+    '''
+    Prints the current game board layout.
+    :param board: dict: positions of the board and what they contain
+    :return: None
+    '''
     print("Here is the current board layout:\n"
-          "\n\tGame Board" + "\t"*4 + "Coordinates\n"
+          "\n\tGame Board" + "\t" * 4 + "Coordinates\n"
           f"\n|\t{board['1,1']}\t{board['1,2']}\t{board['1,3']}\t|"
           "\t|\t(1,1)\t(1,2)\t(1,3)\t|"  # end of line 1
           f"\n|\t{board['2,1']}\t{board['2,2']}\t{board['2,3']}\t|"
@@ -65,22 +92,24 @@ def print_board(board: dict) -> None:
 
 
 def player_instructions(player: int) -> None:
+    '''
+    Print instructions for each player, and identify which player
+    is currently active
+    :param player: int: {1 or 2}
+    :return: None
+    '''
     print(f"Player {player}, enter coordinates"
           "'<row>,<column>' to place your '{PLAYER_SYMBOLS[player]}'"
           "\n\tor enter 'q' to quit the game.")
 
 
-def print_divider(time_delay=False) -> None:
-    if time_delay:
-        for char in DIVIDER:
-            print(char, end="", flush=True)
-            time.sleep(0.015)
-        print("\n")
-    else:
-        print(DIVIDER)
-
-
 def move_is_valid(player_move: str, board: dict) -> bool:
+    '''
+    Identifies whether or not a player has entered a valid coordinate
+    :param player_move: str: coordinate on the game board.
+    :param board: dict: the current game board layout.
+    :return: bool: indicates whether a move is valid (True) or not (False).
+    '''
     if player_move in COORDINATES:
         if board[player_move] == '_':
             return True
@@ -88,27 +117,58 @@ def move_is_valid(player_move: str, board: dict) -> bool:
 
 
 def print_valid_move(player: int) -> None:
+    '''
+    Prints a message to the console indicating the active player has
+    entered a valid move.
+    :param player: int: {1 or 2}
+    :return:
+    '''
     print(f"\nGood move player {player}!")
 
 
 def player_quits(player_move: str) -> bool:
+    '''
+    Identifies whether or not a player has decided to quit the game.
+    :param player_move: str: player input
+    :return: bool: {True or False} indicating whether a player has
+    decided to quit the game.
+    '''
     if re.fullmatch("[Qq]", player_move) or player_move.lower() == 'quit':
         return True
     return False
 
 
 def quit_message(player: int) -> None:
+    '''
+    Print a message to the console stating that the active player has
+    decided to quit the game.
+    :param player: int: {1 or 2}
+    :return: None
+    '''
     print(f"Player {player} has quit the game.")
     time.sleep(1)
 
 
 def invalid_input(input: str) -> None:
+    '''
+    Print a message to the console stating that the player has entered
+    an invalid input
+    :param input: str: player input
+    :return: None
+    '''
     print(f"\nI'm sorry, {input} is not valid."
           " Check that your square is free and try again.\n")
     print_divider(time_delay=True)
 
 
 def is_winner(player_move: str, board: dict) -> bool:
+    '''
+    Identifies whether the player_move has won the game.
+    :param player_move: str: coordinate on the game board.
+    :param board: dict: the current game board set up.
+    :return: bool: {True or False} showing whether or not a player
+    has won the game.
+    '''
     for win_locations in WIN_LOCATIONS.keys():
         winning_sequence = WIN_LOCATIONS[win_locations]
         if player_move in winning_sequence:
@@ -121,6 +181,11 @@ def is_winner(player_move: str, board: dict) -> bool:
 
 
 def end_of_game_message(winner: int) -> None:
+    '''
+    Prints a message to the console stating the winning player.
+    :param winner: int: {1 or 2} The player who has won the game.
+    :return: None
+    '''
     if winner:
         print(f"!!PLAYER {winner} HAS WON THE GAME!!\n"
               f"!!CONGRATULATIONS PLAYER {winner}!!")
@@ -134,7 +199,6 @@ def play() -> None:
     :return: None
     """
     welcome_message()
-    print_divider()
     board = generate_board()
     print_board(board)
     active_player = 1
